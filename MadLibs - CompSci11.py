f643aff0d6688ab79ccbfa8da372ce746d9b92ec
Sachin Gupta <sachinvgupta9@gmail.com>
2024-01-02 00:23:39 -0500

    first commit

---
+++ MadLibs - CompSci11.py
@@ -0,0 +1,39 @@
+#MadLibs! Create a funny customized story!
+
+print()
+
+their_name = input("Please type your name here: ")
+print()
+print("Hello " + their_name + ", you have selected \
+to play Mad Libs, the game where you choose funny words and its created \
+into a funny story to get the whole crowd laughing. You can play Mad Libs with a \
+group or alone but it is fun either way. So what are you waiting for, lets get started! \n")
+
+one_verb = input("Type a weather condition to be in the story: ")
+two_place = input("Type a place near you: ")
+three_plural_noun = input("Type something that you like (plural): ")
+four_noun = input("Type a place or thing (singular): ")
+five_noun = input("Type something that you want (singular): ")
+
+print()
+
+print("Please wait as your words are entered into a story to create a funny Mad Libs story... \n\n")
+
+print("_____________________________\n")
+
+print("CHRISTMAS DAY REALLY IS OKAY \n\n" + "It is " + one_verb + " outside on this beautiful Sunday morning. We all \
+gather here at the " + two_place + " to celebrate the most wonderful day of the year and to gather with friends and family. But, to be \
+honest I'm just here for the gifts and " + three_plural_noun + ". Shush, keep that on the low. \
+I really don’t want anyone else to know that or else I'll be in deep trouble. Me and the other \
+young kids are now starting to play hide and seek. I quickly hide in the " + four_noun + " but it is \
+really disgusting and stinky here. I can tell that I made a big mistake hiding here, but at least I \
+ended up winning the game. I continued to play until it became 6:00 PM, also known as dinner time. For dinner, I \
+ate the most delicious tofurkey and even had a side of perfectly roasted potatoes, yum! Finally, It is now presents time! I \
+went to open my giant present and I am so happy that I got the top present on my Christmas \
+list, a " + five_noun + "! Hohoho hehehe Merry Christmas, and to all a good night!\n")
+
+print("_____________________________\n\n")
+
+FINISH = ("That was your Mad Lib story. I hope you had a laugh and click \"run\" to play again. \n")
+print(FINISH)
+
